e3c34a84abc0d5b67faf4cd8f2654d9105b20f28
pawn-4-git <pawn4sns+dummy@gmail.com>
2026-01-18 11:31:47 +0900

    怪しい計算が含まれるコード

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        function(1, 0);
+    }
+
+    public static void function(int value, int value2) {
+        System.out.println(value / value2);
+
     }
 }
